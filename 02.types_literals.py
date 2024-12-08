8f4de5f222fad745330d8528b1efefb4a35983b7
devhadson <hadson1@gmail.com>
2024-12-08 12:32:53 -0500

    2.2. Section 1 - Types of Literals (string and character)

---
+++ 02.types_literals.py
@@ -0,0 +1,27 @@
+"""
+2.2. Section 1 - Types of Literals in Python:
+Python supports various types of literals, such as numeric literals, string literals, 
+Boolean literals, and more. Let's explore different types of literals in Python with examples:
+"""
+#2.2.1. String Literals
+# in single quote
+x = 'stringforliterals'
+print(x)
+
+# in double quotes
+y = "stringforliterals"
+print(y)
+# multi-line String
+x = '''string 
+           for 
+               literals'''
+print(x)
+
+#2.2.2. Character Literals
+# character literal in single quote
+v = 'n'
+print(v)
+
+# character literal in double quotes
+w = "a"
+print(w)
